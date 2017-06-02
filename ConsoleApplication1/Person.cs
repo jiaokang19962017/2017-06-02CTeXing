248a5fc1befa356a6270ba0b2a8906a4dd3cf378
jiaokang <Administrator@HP201-1>
2017-06-02 11:15:08 +0800

    c#特性

---
+++ ConsoleApplication1/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+
+        public Person()
+        {
+                
+        }
+        public Person(string name,int age,string sex)
+        {
+            this.Age = age;
+            this.Name = name;
+            this.Sex = sex;
+        }
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,38 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+
+            Person p = new Person("张三", 16, "男");//构造函数
+                                                 /*   p.Name = "张三";
+                                                    p.Sex = "男";//属性
+                                                    p.Age = 15;*/
+            Console.WriteLine(p.Name + "," + p.Sex + "," + p.Age);
+            //对象初始化器
+            Person p1 = new Person { Name = "王五", Age = 18, Sex = "女" };
+
+            Person[] array = { new Person { Name = "王五", Age = 18, Sex = "女" }, new Person("田七", 25, "女") };
+
+            List<int> list = new List<int> { 5, 9, 10, 12, 6 };
+            List<string> listr = new List<string>(new string[] { "1", "2", "3" });
+            ArrayList arraylist = new ArrayList();
+
+            List<Person> lsp = new List<Person>();
+            lsp.Add(new Person("你好", 15, "男"));
+            lsp.Add(new Person { Name = "焦康", Sex = "男", Age = 21 });
+
+            List<Person> lsp1 = new List<Person>
+            {
+            new Person("张三",21,"女");
+            new Person("张八", 21, "女");
+            new Person
+            {
+                Name = "王八",
+                Age = 16,
+                Sex = "男"
+            };
         }
+      
+
     }
 }
+
